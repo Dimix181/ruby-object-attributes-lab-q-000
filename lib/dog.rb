c55c5e04eee684437cce464a8b16f133d0791cb1
Dimix181 <dimix181@gmail.com>
2016-05-11 22:29:30 -0400

    final submit

---
+++ lib/dog.rb
@@ -0,0 +1,22 @@
+
+class Dog
+
+ #could also be written  attr_accessor :name, :breed
+
+def name=(dogs_name)
+  @name = dogs_name
+end
+
+def breed=(breed)
+  @breed = breed
+end
+
+def name
+  @name
+end
+
+def breed
+  @breed
+end
+
+end
